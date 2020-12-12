fbc591a33e1ac45ff9ed63dd7d11525e8a59cc73
HenryDev <enrique.quiroz.89@gmail.com>
2020-12-12 07:32:02 -0600

    Verison 1. Agrega datos estaticos

---
+++ app.js
@@ -0,0 +1,34 @@
+// Initialize Cloud Firestore through Firebase
+firebase.initializeApp({
+    apiKey: "AIzaSyC7IxVax8cZ5eLwEhlHE5leNVlX7TBUIQ0",
+    authDomain: "firestorecrud-f8226.firebaseapp.com",
+    projectId: "firestorecrud-f8226"
+  });
+  
+  var db = firebase.firestore();
+
+
+function guardar(){
+/*para pasar los datos de los input a las variables
+ocupamos el DOM */
+
+  var nombre = document.getElementById('nombre').value;
+  var apellido = document.getElementById('apellido').value;
+  var fecha = document.getElementById('fecha').value;
+
+ //aAgregar Documentos
+ db.collection("user").add({
+    first: "Leonel",
+    last: "Polar",
+    born: 1815        })
+.then(function(docRef){
+  console.log("Document written ID ",docRef.id);                
+                  })
+.catch(function(error){
+console.error("Error adding docuemnt: ", error)
+});
+
+}
+
+
+ 
